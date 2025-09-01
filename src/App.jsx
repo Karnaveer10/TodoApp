8fd5a52a312b534e22d3655eae9211cc0a8d15f0
Karnaveer10 <karnaveersisodia@gmail.com>
2025-09-02 01:17:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="h-screen bg-red-500 flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-white">Tailwind Works 🎉</h1>
+      </div>    </>
+  )
+}
+
+export default App
